b3fc17637fcd0a19d6b3ecadef296ae141bc7373
Karorios <hkrios10@ucatolica.edu.co>
2025-04-29 08:15:36 -0500

    Update models.py

--- data/models.py
+++ data/models.py
@@ -1,4 +1,13 @@
-'''Aqui debes consignar el modelo que se te indico en el parcial
-Escribe aquí el que te corresponde.
+from pydantic import BaseModel, Field
+from typing import Optional
 
-'''
+# Modelo para representar una película
+class usuario(BaseModel):
+    id: int
+    nombre: str = Field(..., min_length=1, max_length=50)
+    
+
+# Modelo para la respuesta simplificada de una película
+class usuariorespuesta(BaseModel):
+    nombre: str
+    id: str

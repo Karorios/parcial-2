38a5f2fbb5f8b6737fd8f60f88f8001945e4881b
Karorios <hkrios10@ucatolica.edu.co>
2025-04-29 08:15:50 -0500

    Update operations_db.py

--- operations/operations_db.py
+++ operations/operations_db.py
@@ -1,1 +1,14 @@
-'''Aqui debes construir las operaciones que se te han indicado'''
+import csv
+from typing import List
+from models import usuario
+
+ARCHIVO_USUARIOS = 'csv_data/usuario.csv'
+
+
+# Guardar lista de usuario en CSV
+def guardar_peliculas_csv(usuario: List[usuario]):
+    with (open(ARCHIVO_USUARIOS, mode='w', newline='') as archivo):
+        escritor = csv.writer(archivo)
+        escritor.writerow(usuario.__annotations__.keys())
+        for usuario in usuario:
+            escritor.writerow(usuario.dict().values())
